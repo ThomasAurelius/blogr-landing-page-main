47a77b20ea4ff3953a99e2e33159a8e96170c2a2
Thomas Aurelius <teg824@hotmail.com>
2022-03-19 18:36:24 -0500

    work on menu

--- index.js
+++ index.js
@@ -73,15 +73,46 @@ connect_modal.addEventListener('mouseout', (e) => {
 // mobile menu
 
 const product_menu = document.querySelector('.product-menu-title')
-
 const product_menu_list = document.querySelector('.product-menu-list')
+const company_menu = document.querySelector('.company-menu-title')
+const company_menu_list = document.querySelector('.company-menu-list')
+
+const connect_menu = document.querySelector('.connect-menu-title')
+const connect_menu_list = document.querySelector('.connect-menu-list')
+
 
 
 product_menu.addEventListener('click', () => {
    product_menu_list.classList.toggle('hidden')
+   company_menu_list.classList.add('hidden')
+   connect_menu_list.classList.add('hidden')
+})
+company_menu.addEventListener('click', () => {
+   company_menu_list.classList.toggle('hidden')
+   product.classList.add('hidden')
+   connect_menu_list.classList.add('hidden')
+})
+connect_menu.addEventListener('click', () => {
+   connect_menu_list.classList.toggle('hidden')
+   company_menu_list.classList.add('hidden')
+   product.classList.add('hidden')
 })
 
 
 
 
+menu_button = document.getElementById('menu-icon')
+close_button = document.getElementById('menu-close')
+menu = document.getElementById('mobile-menu')
 
+menu_button.addEventListener('click', () => {
+   menu.classList.remove('hidden')
+   menu_button.classList.add('hidden')
+   close_button.classList.remove('hidden')
+})
+
+close_button.addEventListener('click', () => {
+   menu.classList.add('hidden')
+   menu_button.classList.remove('hidden')
+   close_button.classList.add('hidden')
+})
