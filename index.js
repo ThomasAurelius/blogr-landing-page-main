4ed73c20b831e8a4e330713ceea04584d60c20b3
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-03-18 23:52:52 -0500

    desktop 99% complete

---
+++ index.js
@@ -0,0 +1,80 @@
+const product = document.getElementById('product-title')
+const product_modal = document.getElementById('product-modal')
+
+const company = document.getElementById('company-title')
+const company_modal = document.getElementById('company-modal')
+
+const connect = document.getElementById('connect-title')
+const connect_modal = document.getElementById('connect-modal')
+
+
+
+
+product.addEventListener('mouseover', (e) => {
+   product_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+product_modal.addEventListener('mouseover', (e) => {
+   product_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+product.addEventListener('mouseout', (e) => {
+   product_modal.style.visibility = 'hidden'
+})
+
+product_modal.addEventListener('mouseout', (e) => {
+   product_modal.style.visibility = 'hidden'
+})
+
+
+
+company.addEventListener('mouseover', (e) => {
+   company_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+company_modal.addEventListener('mouseover', (e) => {
+   company_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+company.addEventListener('mouseout', (e) => {
+   company_modal.style.visibility = 'hidden'
+})
+
+company_modal.addEventListener('mouseout', (e) => {
+   company_modal.style.visibility = 'hidden'
+})
+
+
+
+
+
+connect.addEventListener('mouseover', (e) => {
+   connect_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+connect_modal.addEventListener('mouseover', (e) => {
+   connect_modal.style.visibility = 'visible'
+   console.log(e)
+})
+
+connect.addEventListener('mouseout', (e) => {
+   connect_modal.style.visibility = 'hidden'
+})
+
+connect_modal.addEventListener('mouseout', (e) => {
+   connect_modal.style.visibility = 'hidden'
+})
+
+
+
+
+
+
+
+
+
